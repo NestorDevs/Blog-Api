4ead2706794233c33e425dfdd8d94240dfbbf78f
NestorDevs <neivertjosias@gmail.com>
2021-02-24 11:30:36 -0400

    add image header blog entries

--- src/blog/controller/blog.controller.ts
+++ src/blog/controller/blog.controller.ts
@@ -1,7 +1,8 @@
+import { Image } from './../model/image.interface';
 import { UseIsAuthorGuard } from '../guards/user-is-author.guard';
 import { BlogEntry } from './../model/blog-entries.interface';
 import { JwtAuthGuard } from './../../auth/guards/jwt-guard';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { BlogService } from './../service/blog.service';
 import {
   Body,
@@ -14,10 +15,31 @@ import {
   Put,
   Query,
   Request,
+  Res,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import path = require('path');
+import { join } from 'path';
 
 export const BLOG_ENTRIES_URL = 'http://localhost:3000/api/blog-entries';
+
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads/blog-entry-images',
+    filename: (req, file, cb) => {
+      const filename: string =
+        path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`);
+    },
+  }),
+};
 @Controller('blog-entries')
 export class BlogController {
   constructor(private blogService: BlogService) {}
@@ -84,5 +106,21 @@ export class BlogController {
   @Delete(':id')
   deleteOne(@Param('id') id: number): Observable<any> {
     return this.blogService.deleteOne(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('image/upload')
+  @UseInterceptors(FileInterceptor('file', storage))
+  uploadFile(@UploadedFile() file, @Request() req): Observable<Image> {
+    return of(file);
+  }
+
+  @Get('image/:imagename')
+  findImage(@Param('imagename') imagename, @Res() res): Observable<Object> {
+    return of(
+      res.sendFile(
+        join(process.cwd(), 'uploads/blog-entry-images/' + imagename),
+      ),
+    );
   }
 }
